ab42d9f543bc258d2ea5c7ed041795f37ac6b661
Nabheet Madan <nabheet.cloud@gmail.com>
2018-05-01 19:33:01 +0530

    100daysofcode Swift Learning

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+var a = 1
+var b = 1
+
+a = 2
+
+str = "hello"
+
+var numberOfApples = 19
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+// constants
+
+let con = "hello"
+
+
