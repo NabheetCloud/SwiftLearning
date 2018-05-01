1b0a891613b2c421893ede0c90457755fce2504c
Nabheet Madan <nabheet.cloud@gmail.com>
2018-05-01 19:43:30 +0530

    100daysofcode Swift Learning

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,20 +1,27 @@
 //: Playground - noun: a place where people can play
-
+// Different Data Type in Swift
 import UIKit
 
-var str = "Hello, playground"
+var str:String = "Hello, playground"
 
 print(str)
 
-var a = 1
-var b = 1
+var a:Int = 1
+var b:Int = 1
 
 a = 2
 
-str = "hello"
+str = String(29)
 
 var numberOfApples = 19
 
+var c:Float = 2.3
+var d:Double = 10.702
+var e:Bool = true
+
+print(Int(round(d)))
+print(Int(d))
+print(Int(c))
 print(a+b)
 print(a-b)
 print(a*b)
